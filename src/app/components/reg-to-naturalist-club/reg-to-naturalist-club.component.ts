a28a889fcbc2c09682aa1b3854b1d38e92c4e70f
Mantas Luk <mantasluk7@gmail.com>
2022-05-02 21:56:55 +0300

    add/remove allergy

--- src/app/components/reg-to-naturalist-club/reg-to-naturalist-club.component.ts
+++ src/app/components/reg-to-naturalist-club/reg-to-naturalist-club.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reg-to-naturalist-club',
@@ -15,7 +15,8 @@ export class RegToNaturalistClubComponent implements OnInit {
       'name':new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(16)]),
       'surname':new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(16)]),
       'email':new FormControl(null, [Validators.required, Validators.email]),
-      'grade':new FormControl(null, [Validators.required, this.checkGrade])
+      'grade':new FormControl(null, [Validators.required, this.checkGrade]),
+      'allergy':new FormArray([])
     });
   }
 
@@ -33,5 +34,18 @@ export class RegToNaturalistClubComponent implements OnInit {
     }else{
       return {'gradeIncorrect':true}
     }
+  }
+
+  addAllergy(){
+    const input=new FormControl(null, Validators.required);
+    (<FormArray>this.natureClubForm.get('allergy')).push(input);
+  }
+
+  removeAllergy(){
+    (<FormArray>this.natureClubForm.get('allergy')).removeAt((<FormArray>this.natureClubForm.get('allergy')).length-1);
+  }
+
+  get allergies(){
+    return (<FormArray>this.natureClubForm.get('allergy')).controls;
   }
 }
